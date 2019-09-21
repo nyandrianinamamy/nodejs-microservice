6973319f0352a71ce76643c81cd854dc14bd6322
Razafintsialonina Mamy <nyandrianinamamy@gmail.com>
2019-09-21 11:52:38 +0300

    [TEST] moved fakeDb to correct test directory

--- src/repository/index.ts
+++ src/repository/index.ts
@@ -1,24 +1,5 @@
-import { IDb, NOT_IMPLEMENTED } from './base.repository';
+import { fakeDb } from '../../__test__/fixtures/db.fixture';
 import { UserRepository } from './user.repository';
-
-// Fake db to test everything quickly
-// tslint:disable: no-any
-const fakeDb: IDb = {
-    find: (conditions: any): Promise<any[]> =>
-        Promise.resolve(['user1', 'user2']),
-    findOne: (id: string) => {
-        throw new Error(NOT_IMPLEMENTED);
-    },
-    create: (item: any) => {
-        throw new Error(NOT_IMPLEMENTED);
-    },
-    delete: (id: string) => {
-        throw new Error(NOT_IMPLEMENTED);
-    },
-    update: (id: string, item: any) => {
-        throw new Error(NOT_IMPLEMENTED);
-    },
-};
 
 // Connect the user repository to the db
 const userRepository: UserRepository = new UserRepository(fakeDb);
