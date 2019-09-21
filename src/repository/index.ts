9169c4bfecf038e3921bd3ced184b6495657d3a7
Razafintsialonina Mamy <nyandrianinamamy@gmail.com>
2019-09-21 09:10:48 +0300

    [REPOSITORY] create fakeDb for testing user repository

--- src/repository/index.ts
+++ src/repository/index.ts
@@ -1,10 +1,26 @@
-import { UserRepositoryBuilder } from './user.repository';
+import { IDb, NOT_IMPLEMENTED } from './base.repository';
+import { UserRepository } from './user.repository';
 
-const repository = {
-  find: (conditions: any) => Promise.resolve(['user'])
+// Fake db to test everything quickly
+// tslint:disable: no-any
+const fakeDb: IDb = {
+    find: (conditions: any): Promise<any[]> =>
+        Promise.resolve(['user1', 'user2']),
+    findOne: (id: string) => {
+        throw new Error(NOT_IMPLEMENTED);
+    },
+    create: (item: any) => {
+        throw new Error(NOT_IMPLEMENTED);
+    },
+    delete: (id: string) => {
+        throw new Error(NOT_IMPLEMENTED);
+    },
+    update: (id: string, item: any) => {
+        throw new Error(NOT_IMPLEMENTED);
+    },
 };
-const userRepository: UserRepositoryBuilder = new UserRepositoryBuilder({
-  baseRepository: repository
-});
+
+// Connect the user repository to the db
+const userRepository: UserRepository = new UserRepository(fakeDb);
 
 export { userRepository };
