8b9b3d1bbe50a0c589389b289e8ed3dd5967bdb4
Razafintsialonina Mamy <nyandrianinamamy@gmail.com>
2019-09-21 09:09:00 +0300

    [ENTITY] export userBuilder instead of makeUser  to avoid bound-function

--- src/entity/index.ts
+++ src/entity/index.ts
@@ -2,11 +2,11 @@ import crypto from 'crypto';
 import { UserBuilder } from './user.entity';
 
 const md5 = (text: string) =>
-  crypto
-    .createHash('md5')
-    .update(text)
-    .digest('hex');
+    crypto
+        .createHash('md5')
+        .update(text)
+        .digest('hex');
 
-const makeUser = new UserBuilder({ hasher: md5 }).makeUser;
+const userBuilder = new UserBuilder({ hasher: md5 });
 
-export { makeUser };
+export { userBuilder };
