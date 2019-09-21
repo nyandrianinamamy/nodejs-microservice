dc443fc990e67e836a517b897260d4d9fa0c347d
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2019-09-21 22:21:21 +0300

    [APP] Replace console log by winston logger, replace log message's format #config

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import logger from './config/logger.config';
 
 const server = express();
 
@@ -6,4 +7,6 @@ server.use('/_health', (req, res) => {
     res.status(200).json({ uptime: process.uptime() });
 });
 
-server.listen(4004, () => console.log('Running at localhost:4004'));
+server.listen(4004, () =>
+    logger.info('server.handler Running at localhost:4004'),
+);
