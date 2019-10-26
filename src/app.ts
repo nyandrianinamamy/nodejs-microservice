a43a26cf114494da9bcb05cdd26771e8659759dc
Razafintsialonina Mamy <nyandrianinamamy@gmail.com>
2019-10-26 12:52:45 +0300

    [APP] add logger inside event handler #master

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import morgan from 'morgan';
 import { appRouter } from './app.routes';
 import logger, { morganStream } from './config/logger.config';
 
+// tslint:disable: no-console
 const server = express();
 
 server.use(bodyParser.json());
@@ -15,25 +16,22 @@ server.use('/_health', (req, res) => {
 
 server.use('/api/v1', appRouter);
 
-server.listen(4004, () =>
-    logger.info('server.handler Running at localhost:4004'),
-);
+server.listen(4004, () => logger.info('Running at localhost:4004'));
 
 process.on('SIGINT', () => {
-    // tslint:disable-next-line: no-console
+    logger.info(`Process killed manually`);
     console.log(`Ooooh Looks like you've killed the process manually`);
     process.exit(1);
 });
 
 process.on('exit', (code) => {
-    // tslint:disable-next-line: no-console
+    logger.info(`Process exited with code ${code}`);
     console.log(`Good bye dude, I'm going to exit with code ${code}`);
 });
 
 process.on('uncaughtException', (err) => {
-    // tslint:disable: no-console
+    logger.error(`Uncaught exception ${err.stack}`);
     console.error('Ouuuuuch some uncaught exceptions occured');
     console.error(err);
-
     process.exit(1);
 });
