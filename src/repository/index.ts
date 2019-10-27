379be9f41b02ec9cd0e09fe6ee1f3b6208bea9f0
Razafintsialonina Mamy <nyandrianinamamy@gmail.com>
2019-10-27 13:36:41 +0300

    [REPOSITORY] removed base.repository #master

--- src/repository/index.ts
+++ src/repository/index.ts
@@ -1,7 +1,7 @@
-import { fakeUserDb } from '../../__test__/fixtures/user.fixture';
+import { userModel } from '../entity/user.mongoose.schema';
 import { UserRepository } from './user.repository';
 
 // Connect the user repository to the db
-const userRepository: UserRepository = new UserRepository(fakeUserDb);
+const userRepository: UserRepository = new UserRepository(userModel);
 
 export { userRepository };
