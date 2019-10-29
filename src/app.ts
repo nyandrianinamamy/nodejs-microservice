4b62da8dd4baf9475946d88318e0a488751b138b
Razafintsialonina Mamy <nyandrianinamamy@gmail.com>
2019-10-29 10:07:18 +0300

    [APP] remove event listener on uncaughtException #master

--- src/app.ts
+++ src/app.ts
@@ -2,23 +2,13 @@ import { database, server } from './config';
 import logger from './config/logger.config';
 
 database.connect(() => {
-    // tslint:disable: no-console
     server.init();
     process.on('SIGINT', () => {
         logger.info(`Process killed manually`);
-        console.log(`Ooooh Looks like you've killed the process manually`);
         process.exit(1);
     });
 
     process.on('exit', (code) => {
         logger.info(`Process exited with code ${code}`);
-        console.log(`Good bye dude, I'm going to exit with code ${code}`);
-    });
-
-    process.on('uncaughtException', (err) => {
-        logger.error(`Uncaught exception ${err.stack}`);
-        console.error('Ouuuuuch some uncaught exceptions occured');
-        console.error(err);
-        process.exit(1);
     });
 });
