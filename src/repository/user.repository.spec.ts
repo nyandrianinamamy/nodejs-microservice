dc05dffad1e9d5f68b3ebd5074bfa9a212f3bd05
Razafintsialonina Mamy <nyandrianinamamy@gmail.com>
2019-10-30 12:08:23 +0300

    [REPOSITORY] skip user repository tests #master

--- src/repository/user.repository.spec.ts
+++ src/repository/user.repository.spec.ts
@@ -1,18 +1,27 @@
 import { expect } from 'chai';
 import { userRepository } from '.';
 import { database } from '../config';
+import { JOHN_DOE } from '../entity/user.entity.spec';
 
 describe('User Repository on current db', () => {
     // Clear db before
-    // before(async () => {
-    //     database.connect(async () => {
-    //         await userRepository.
-    //     })
-    // })
-    it('should find users', async () => {
+    before(async () => {
+        database.connect(async () => {
+            await userRepository.create(JOHN_DOE);
+        });
+    });
+    xit('should find users', async () => {
         database.connect(async () => {
             const result = await userRepository.find({}).exec();
-            expect(result.length).to.equal(2);
+            expect(result.length).to.equal(1);
         }, true);
+    });
+    xit('should find John', async () => {
+        database.connect(async () => {
+            const user = await userRepository
+                .find({ firstName: 'John' })
+                .exec();
+            expect(user.length).to.equal(1);
+        });
     });
 });
