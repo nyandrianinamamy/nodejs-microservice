e6e5cbb10b45bb7494c5fabaa721d083c3e0b346
Razafintsialonina Mamy <nyandrianinamamy@gmail.com>
2019-10-30 11:56:23 +0300

    [REPOSITORY] add UserModel interface #master

--- src/repository/user.repository.ts
+++ src/repository/user.repository.ts
@@ -1,5 +1,5 @@
 import { IUser } from '../entity/user.entity';
+import { IUserModel } from './../entity/user.mongoose.schema';
 import { MongoRepository } from './mongo.repository';
-
 // tslint:disable-next-line: no-empty-interface
-export class UserRepository extends MongoRepository<IUser> {}
+export class UserRepository extends MongoRepository<IUserModel, IUser> {}
