0675dc3b5a489474cb4522630ec15fb32eeae5ed
Razafintsialonina Mamy <nyandrianinamamy@gmail.com>
2019-11-06 10:13:34 +0300

    [TEST] created db handler for mongo testing #master

---
+++ src/tests/db.handler.ts
@@ -0,0 +1,33 @@
+import { MongoMemoryServer } from 'mongodb-memory-server-core';
+import mongoose from 'mongoose';
+
+const mongod = new MongoMemoryServer();
+
+const connect = async () => {
+    const uri = await mongod.getConnectionString();
+    const mongooseOpts = {
+        useNewUrlParser: true,
+        autoReconnect: true,
+        reconnectTries: Number.MAX_VALUE,
+        reconnectInterval: 1000,
+        useUnifiedTopology: true,
+    };
+    await mongoose.connect(uri, mongooseOpts);
+};
+
+const closeDatabase = async () => {
+    await mongoose.connection.dropDatabase();
+    await mongoose.connection.close();
+    await mongod.stop();
+};
+
+const clearDatabase = async () => {
+    const collections = mongoose.connection.collections;
+    // tslint:disable-next-line: forin
+    for (const key in collections) {
+        const collection = collections[key];
+        await collection.deleteMany({});
+    }
+};
+
+export { connect, closeDatabase, clearDatabase };
