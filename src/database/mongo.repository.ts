54db35c8f472f78d5a8bc42b658c207ede73cef2
Razafintsialonina Mamy <nyandrianinamamy@gmail.com>
2019-11-21 10:11:22 +0300

    [REPOSITORY] add filter and search to repository #master

--- src/database/mongo.repository.ts
+++ src/database/mongo.repository.ts
@@ -1,4 +1,5 @@
 import { Document, DocumentQuery, Model } from 'mongoose';
+import { buildFilter, buildSearch } from '../utils/query.utils';
 import { IRead } from './interfaces/read.interface';
 import { IWrite } from './interfaces/write.interface';
 
@@ -7,9 +8,9 @@ export type IDb<D extends Document, I> = IRead<D, I> & IWrite<D, I>;
 
 export const NOT_IMPLEMENTED = 'Repository Method not implemented.';
 
-export abstract class MongoRepository<D extends Document, I>
-    implements IRead<D, I>, IWrite<D, I> {
+export abstract class MongoRepository<D extends Document, I> implements IRead<D, I>, IWrite<D, I> {
     model: Model<any>;
+    query = {};
     constructor(model: Model<D>) {
         this.model = model;
     }
@@ -21,15 +22,31 @@ export abstract class MongoRepository<D extends Document, I>
         return this.model.deleteOne({ _id: id }).then(() => true);
     }
 
+    filter(filter: Record<string, any>) {
+        this.query = { ...this.query, ...buildFilter(filter) };
+        return this;
+    }
+
     find(conditions: any): DocumentQuery<D[], D> {
         return this.model.find(conditions);
+    }
+
+    findAndExec() {
+        const query = this.query;
+        this.query = {};
+        return this.model.find(query).exec();
     }
 
     findOne(id: string): DocumentQuery<D | null, D> {
         return this.model.findById(id);
     }
 
+    search(search: string) {
+        this.query = { ...this.query, ...buildSearch(search) };
+        return this;
+    }
+
     update(id: string, item: Partial<I>): Promise<D | null> {
-        return this.model.findByIdAndUpdate(id, item, { new: true }).exec();
+        return this.model.findOneAndUpdate({ _id: id }, item).exec();
     }
 }
