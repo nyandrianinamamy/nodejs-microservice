f4e654b8d23b0d170f70b89deaed9ead2d2b7b6c
Razafintsialonina Mamy <nyandrianinamamy@gmail.com>
2019-11-22 14:05:46 +0300

    [CONFIG] create env default method #master

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,17 +1,31 @@
 import * as dotenv from 'dotenv';
+import * as envvar from 'env-var';
 
 dotenv.config();
-let path;
-switch (process.env.NODE_ENV) {
-    case 'test':
-        path = `${__dirname}/../../.env.test`;
-        break;
-    case 'production':
-        path = `${__dirname}/../../.env.production`;
-        break;
-    default:
-        path = `${__dirname}/../../.env`;
-}
-dotenv.config({ path });
-
+export const env = {
+    server: {
+        port: envvar.get('NODE_PORT').asIntPositive() || 4004
+    },
+    mongodb: {
+        dbURI: envvar.get('MONGODB_URI').asUrlString() || 'mongodb://localhost:27017/testenv'
+    },
+    jwt: {
+        secretKey: envvar.get('JWT_SECRET').asString() || 'my-special-secret-key',
+        expiration: envvar.get('JWT_EXPIRATION').asIntPositive() || 20000
+    },
+    initialize() {
+        let path;
+        switch (process.env.NODE_ENV) {
+            case 'test':
+                path = `${__dirname}/../../.env.test`;
+                break;
+            case 'production':
+                path = `${__dirname}/../../.env.production`;
+                break;
+            default:
+                path = `${__dirname}/../../.env`;
+        }
+        dotenv.config({ path });
+    }
+};
 export const DEBUG_LEVEL = process.env.LOG_LEVEL;
