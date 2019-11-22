cf9da995f659bda3bcd1808d7317b097bd0c2f61
Razafintsialonina Mamy <nyandrianinamamy@gmail.com>
2019-11-22 12:47:24 +0300

    [APP] create authentication app config #master

---
+++ src/app/app.authentication.ts
@@ -0,0 +1,33 @@
+import passport from 'passport';
+import { ExtractJwt, Strategy as JwtStrategy } from 'passport-jwt';
+import { Strategy as LocalStrategy } from 'passport-local';
+import { comparePassword } from '../utils/password.utils';
+import { userRepository } from './../user/repository';
+
+const localStrategy = new LocalStrategy({ usernameField: 'email' }, async (email, password, done) => {
+    const user = await userRepository.findOne({ email }).exec();
+    if (!user) {
+        return done(null, false, { message: 'Utilisateur inexistant' });
+    }
+    const validPassword = await comparePassword(password, user.password);
+    if (!validPassword) {
+        return done(null, false, { message: 'Mot de passe incorrect' });
+    }
+    return done(null, user);
+});
+
+const jwtStrategy = new JwtStrategy(
+    {
+        secretOrKey: process.env.JWT_SECRET,
+        jwtFromRequest: ExtractJwt.fromExtractors([ExtractJwt.fromAuthHeaderAsBearerToken()])
+    },
+    async (payload, done) => {
+        const user = await userRepository.findOne({ _id: payload._id }).exec();
+        return user ? done(null, user) : done(null, false);
+    }
+);
+
+passport.use(localStrategy);
+passport.use(jwtStrategy);
+
+export default passport;
